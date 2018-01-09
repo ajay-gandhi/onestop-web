816d4fba98be72b31c155c79596545fff39685d2
Ajay Gandhi <aag255@cornell.edu>
2018-01-08 16:41:26 -0800

    Checkpoint - gotta make markers from straight up images

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,15 +1,20 @@
+import "components/scss/Map.scss";
+
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import GoogleMap from "google-map-react";
 import Icon from "components/Icon";
+import Bus from "components/Bus";
 
-const StopMarker = (props) => (
-  <div className="StopMarker">
-    <Icon glyph={ props.glyph } />
-  </div>
-);
+const StopMarker = (props) => {
+  return (
+    <div className="StopMarker">
+      <Icon glyph="directions_bus" />
+    </div>
+  );
+};
 
 class Map extends React.Component {
   static propTypes = {
@@ -28,22 +33,37 @@ class Map extends React.Component {
     zoom: 11,
   };
 
+  componentWillReceiveProps = (nextProps) => {
+    const markers = Object.values(nextProps.stops).map((stop) => {
+      return new this.mapApi.Marker({
+        position: new this.mapApi.LatLng(stop.lat, stop.lng),
+        map: this.map,
+        title: stop.name,
+        icon: "https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/T1E8AWuYkAS.png",
+        // icon: {
+          // path: Bus,
+        // },
+      });
+    });
+  }
+
+  setMap = ({ map, maps }) => {
+    this.map = map;
+    this.mapApi = maps;
+  }
+
   render = () => {
-    const stops = Object.values(this.props.stops).map((stop) => (
-      <StopMarker
-        key={ stop.id }
-        lat={ stop.lat }
-        lng={ stop.lng }
-        glyph="directions_bus"
-      />
-    ));
+    // const stops = Object.values(this.props.stops).map((stop) => (
+      // <StopMarker key={ stop.id } lat={ stop.lat } lng={ stop.lng } />
+    // ));
+
     return (
       <div className="MapContainer" style={ { width: "400px", height: "400px" } }>
         <GoogleMap
           defaultCenter={ this.props.center }
           defaultZoom={ this.props.zoom }
+          onGoogleApiLoaded={ this.setMap }
         >
-          { stops }
         </GoogleMap>
       </div>
     );

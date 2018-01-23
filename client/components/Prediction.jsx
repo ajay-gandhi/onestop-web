81421a7e5774488ae9bfe1d01e366c3f25d399dc
Ajay Gandhi <aag255@cornell.edu>
2018-01-23 15:11:38 -0800

    Style prediction

--- client/components/Prediction.jsx
+++ client/components/Prediction.jsx
@@ -1,9 +1,11 @@
+import "components/scss/Prediction.scss";
+
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { actions } from "components/store/Store";
 
-import { Button } from "react-bootstrap";
+import Icon from "components/Icon";
 
 class Prediction extends React.PureComponent {
   static propTypes = {
@@ -14,26 +16,40 @@ class Prediction extends React.PureComponent {
     getPredictions: PropTypes.func,
   };
 
+  componentWillReceiveProps = (nextProps) => {
+    if (this.props.step !== 3 && nextProps.step === 3) {
+      this.props.getPredictions();
+    }
+  }
+
   render = () => {
     let predictionMessage;
     if (this.props.predictions && this.props.predictions._title) {
       const prediction = this.props.predictions.prediction[0];
       predictionMessage = (
-        <div>
-          <h4>Next vehicle arrives in { prediction._minutes } minutes</h4>
-          <span>Direction: { this.props.predictions._title }</span>
+        <div className="Prediction__content">
+          <h4 className="Prediction__content__minutes Prediction__verticalCenter">
+            <Icon glyph="access_time" className="Prediction__content__icon" />
+            Next vehicle arrives in { prediction._minutes } minutes
+          </h4>
+          <div className="Prediction__verticalCenter">
+            <Icon glyph="near_me" className="Prediction__content__icon" />
+            Direction: { this.props.predictions._title }
+          </div>
+        </div>
+      );
+    } else if (!this.props.isFetching) {
+      predictionMessage = (
+        <div className="Prediction__content Prediction__verticalCenter">
+          <Icon glyph="do_not_disturb" className="Prediction__content__icon" />
+          No prediction for this stop.
         </div>
       );
     }
 
     return (
-      <div>
-        <Button
-          onClick={ this.props.getPredictions }
-          disabled={ !this.props.selectedStop || this.props.isFetching }
-        >
-          Get sample prediction
-        </Button>
+      <div className="Prediction">
+        <h3 className="Prediction__title">Sample Prediction</h3>
         { predictionMessage }
       </div>
     );
